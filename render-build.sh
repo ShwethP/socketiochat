88c7179e8b7c6a5664b560dd2fe0c9ed5dca7d2e
Shweta.Patil <shweta.patil@deltaww.com>
2025-04-04 17:44:12 +0530

    Add render-build.sh to build sqlite3 from source

--- render-build.sh
+++ render-build.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
+#!/usr/bin/env bash
+echo "Installing dependencies from scratch..."
+rm -rf node_modules package-lock.json
+npm install --build-from-source sqlite3
+
 npm install --force
